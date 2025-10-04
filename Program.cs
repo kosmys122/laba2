31665394c10ba5ca2636ebcc2c68101dba7a3788
Шапкин Николай <kosmys122@gmail.com>
2025-10-04 23:30:04 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@ namespace laba2
 {
     internal class Program
     {
-
+        
         static void Main(string[] args)
         {
-
+            //Задание 1
+            Console.Write("Число:");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+                Console.WriteLine("Число не чётное");
         }
     }
 }
